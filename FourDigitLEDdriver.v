31e54aef903ea17150eaf1c4fd7cba011ca4f238
BiliouriV <58600881+BiliouriV@users.noreply.github.com>
2019-12-08 18:33:01 +0200

    Update FourDigitLEDdriver.v

--- FourDigitLEDdriver.v
+++ FourDigitLEDdriver.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: University of Thessaly
+// Engineer: Viktoria Biliouri
 // 
 // Create Date:    01:55:46 10/07/2018 
 // Design Name: 
